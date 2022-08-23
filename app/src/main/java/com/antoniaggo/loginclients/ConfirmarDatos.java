8a7562025090f7eb55ac2f939ffe8094183cc4a4
Antonia <antonia.gallardo.gomez@gmail.com>
2022-08-23 22:13:37 +0200

    Bug fixed MainActivity Code and ConfirmarDatos Design and Code

--- app/src/main/java/com/antoniaggo/loginclients/ConfirmarDatos.java
+++ app/src/main/java/com/antoniaggo/loginclients/ConfirmarDatos.java
@@ -2,13 +2,83 @@ package com.antoniaggo.loginclients;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ConfirmarDatos extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+
+public class ConfirmarDatos extends AppCompatActivity implements
+        View.OnClickListener{
+
+    private TextInputEditText tietNombreConfirmarDatos;
+    private TextInputEditText tietFechaConfirmarDatos;
+    private TextInputEditText tietTelefonoConfirmarDatos;
+    private TextInputEditText tietEmailConfirmarDatos;
+    private TextInputEditText tietDescripcionContactoConfirmarDatos;
+
+    private Button btn_editarConfirmarDatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_datos);
+
+        // Recogemos los extras que nos envía la Activity MainActivity.class
+        Bundle parametros = getIntent().getExtras();
+        // getString(getResources().getString(R.string.pnombre)) equivale a poner pnombre que está en strings.xml
+        String nombre = parametros.getString(getResources().getString(R.string.pnombre));
+        String fecha = parametros.getString(getResources().getString(R.string.pfecha));
+        String telefono = parametros.getString(getResources().getString(R.string.ptelefono));
+        String email = parametros.getString(getResources().getString(R.string.pemail));
+        String descripcionContacto = parametros.getString(getResources().getString(R.string.pdescripcion));
+
+        tietNombreConfirmarDatos = findViewById(R.id.tietNombreConfirmarDatos);
+        tietFechaConfirmarDatos = findViewById(R.id.tietFechaConfirmarDatos);
+        tietTelefonoConfirmarDatos = findViewById(R.id.tietTelefonoConfirmarDatos);
+        tietEmailConfirmarDatos = findViewById(R.id.tietEmailConfirmarDatos);
+        tietDescripcionContactoConfirmarDatos = findViewById(R.id.tietDescripcionContactoConfirmarDatos);
+        btn_editarConfirmarDatos = findViewById(R.id.btn_editarConfirmarDatos);
+
+        tietNombreConfirmarDatos.setText(nombre);
+        tietFechaConfirmarDatos.setText(fecha);
+        tietTelefonoConfirmarDatos.setText(telefono);
+        tietEmailConfirmarDatos.setText(email);
+        tietDescripcionContactoConfirmarDatos.setText(descripcionContacto);
+
+        btn_editarConfirmarDatos.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == btn_editarConfirmarDatos) {
+
+            /*
+             * Para cambiar de pantalla de MainActivity a DetalleContacto
+             * al pulsar sobre un ítem de MainActivity
+             */
+            Intent intent = new Intent(ConfirmarDatos.this, MainActivity.class);
+            /*
+             * Para enviar los datos de un usuario a la Activity ConfirmarDatos
+             */
+            intent.putExtra(getResources().getString(R.string.pnombre), tietNombreConfirmarDatos.getText().toString());
+            intent.putExtra(getResources().getString(R.string.pfecha), tietFechaConfirmarDatos.getText().toString());
+            intent.putExtra(getResources().getString(R.string.ptelefono), tietTelefonoConfirmarDatos.getText().toString());
+            intent.putExtra(getResources().getString(R.string.pemail), tietEmailConfirmarDatos.getText().toString());
+            intent.putExtra(getResources().getString(R.string.pdescripcion), tietDescripcionContactoConfirmarDatos.getText().toString());
+
+            startActivity(intent);
+            /* Elimino esta Activity para que no se vayan congelando y guardando en background ya
+             * que es como se comporta por defecto.  Una activity encima de la otra,ect.
+             * Con esta forma de programar sólo tengo siempre una Activity ejecutándose
+             * y mi aplicación no se ralentiza a medida que el usuario la va usando
+             * ya que no se van superponiendo las activies una encima de la otra
+             */
+            finish();
+
+        }
     }
 }
--- app/src/main/java/com/antoniaggo/loginclients/MainActivity.java
+++ app/src/main/java/com/antoniaggo/loginclients/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity implements
         View.OnClickListener {
 
     Button btnDatePicker, btnSiguiente;
-    TextInputEditText tietNombre, tietFecha, tietTelefono, tietDescripcionContacto;
+    TextInputEditText tietNombre, tietFecha, tietEmail, tietTelefono, tietDescripcionContacto;
     int  mYear, mMonth, mDay;
 
 
@@ -25,10 +26,10 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tietFecha = findViewById(R.id.tietFecha);
         tietNombre = findViewById(R.id.tietNombre);
         tietFecha = findViewById(R.id.tietFecha);
         tietTelefono = findViewById(R.id.tietTelefono);
+        tietEmail = findViewById(R.id.tietEmail);
         tietDescripcionContacto = findViewById(R.id.tietDescripcionContacto);
 
         btnDatePicker = findViewById(R.id.btn_date);
@@ -36,6 +37,25 @@ public class MainActivity extends AppCompatActivity implements
 
         btnDatePicker.setOnClickListener(this);
         btnSiguiente.setOnClickListener(this);
+
+    try{
+        // Recogemos los extras que nos envía la Activity MainActivity.class
+        Bundle parametros = getIntent().getExtras();
+        // getString(getResources().getString(R.string.pnombre)) equivale a poner pnombre que está en strings.xml
+        String nombre = parametros.getString(getResources().getString(R.string.pnombre));
+        String fecha = parametros.getString(getResources().getString(R.string.pfecha));
+        String telefono = parametros.getString(getResources().getString(R.string.ptelefono));
+        String email = parametros.getString(getResources().getString(R.string.pemail));
+        String descripcionContacto = parametros.getString(getResources().getString(R.string.pdescripcion));
+
+        tietNombre.setText(nombre);
+        tietFecha.setText(fecha);
+        tietTelefono.setText(telefono);
+        tietEmail.setText(email);
+        tietDescripcionContacto.setText(descripcionContacto);
+        }catch(Exception e){
+            Toast.makeText(getBaseContext(),"Error"+e, Toast.LENGTH_SHORT).show();
+        }
     }
 
 
@@ -56,27 +76,29 @@ public class MainActivity extends AppCompatActivity implements
             datePickerDialog.show();
         }
         if (v == btnSiguiente) {
-            /*
-             * Para cambiar de pantalla de MainActivity a DetalleContacto
-             * al pulsar sobre un ítem de MainActivity
-             */
-            Intent intent = new Intent(MainActivity.this,ConfirmarDatos.class);
-            /*
-             * Para enviar los datos de un usuario a la Activity ConfirmarDatos
-             */
-            intent.putExtra(getResources().getString(R.string.pnombre), tietFecha.getText().toString());
-            intent.putExtra(getResources().getString(R.string.ptelefono), tietFecha.getText().toString());
-            intent.putExtra(getResources().getString(R.string.pemail), tietFecha.getText().toString());
-            intent.putExtra(getResources().getString(R.string.pfecha), tietFecha.getText().toString());
 
-            startActivity(intent);
-            /* Elimino esta Activity para que no se vayan congelando y guardando en background ya
-             * que es como se comporta por defecto.  Una activity encima de la otra,ect.
-             * Con esta forma de programar sólo tengo siempre una Activity ejecutándose
-             * y mi aplicación no se ralentiza a medida que el usuario la va usando
-             * ya que no se van superponiendo las activies una encima de la otra
-             */
-            finish();
+                /*
+                 * Para cambiar de pantalla de MainActivity a DetalleContacto
+                 * al pulsar sobre un ítem de MainActivity
+                 */
+                Intent intent = new Intent(MainActivity.this,ConfirmarDatos.class);
+                /*
+                 * Para enviar los datos de un usuario a la Activity ConfirmarDatos
+                 */
+                intent.putExtra(getResources().getString(R.string.pnombre), tietNombre.getText().toString());
+                intent.putExtra(getResources().getString(R.string.pfecha), tietFecha.getText().toString());
+                intent.putExtra(getResources().getString(R.string.ptelefono), tietTelefono.getText().toString());
+                intent.putExtra(getResources().getString(R.string.pemail), tietEmail.getText().toString());
+                intent.putExtra(getResources().getString(R.string.pdescripcion), tietDescripcionContacto.getText().toString());
+
+                startActivity(intent);
+                /* Elimino esta Activity para que no se vayan congelando y guardando en background ya
+                 * que es como se comporta por defecto.  Una activity encima de la otra,ect.
+                 * Con esta forma de programar sólo tengo siempre una Activity ejecutándose
+                 * y mi aplicación no se ralentiza a medida que el usuario la va usando
+                 * ya que no se van superponiendo las activies una encima de la otra
+                 */
+                finish();
 
         }
     }
